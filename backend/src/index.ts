45c1ff11d63d7b90a30b9994b5cc36dacb201ba9
Igor Bernardes Seixas <igor.bernardes2@gmail.com>
2025-08-11 18:15:13 -0300

    feat: servir pasta uploads corretamente para exibir imagens

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,14 +10,27 @@ interface MulterRequest extends Request {
   file?: Express.Multer.File;
 }
 
-const upload = multer({ dest: "uploads/" });
+// const upload = multer({ dest: "uploads/" });
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/"); // Pasta onde salvar
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname); // Pega a extensão original (.jpg, .png, etc)
+    const uniqueName = Date.now() + "-" + Math.round(Math.random() * 1e9) + ext;
+    cb(null, uniqueName);
+  },
+});
+
+export const upload = multer({ storage });
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 // Serve a pasta uploads para acessar as imagens
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/uploads", express.static(path.resolve("uploads")));
 
 // Conectar ao MongoDB
 mongoose
@@ -32,6 +45,20 @@ app.get(
     try {
       const cars: ICar[] = await Car.find({});
       res.json(cars);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// Buscar carro por ID
+app.get(
+  "/api/cars/:id",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const car = await Car.findById(req.params.id);
+      if (car) res.json(car);
+      else res.status(404).json({ error: "Carro não encontrado" });
     } catch (error) {
       next(error);
     }
@@ -60,20 +87,6 @@ app.post(
 
       const savedCar = await newCar.save();
       res.json(savedCar);
-    } catch (error) {
-      next(error);
-    }
-  }
-);
-
-// Buscar carro por ID
-app.get(
-  "/api/cars/:id",
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const car = await Car.findById(req.params.id);
-      if (car) res.json(car);
-      else res.status(404).json({ error: "Carro não encontrado" });
     } catch (error) {
       next(error);
     }
