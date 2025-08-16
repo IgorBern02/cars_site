c20bb961d90a8c6e2b60d47b60ecab9b49682eaf
Igor Bernardes Seixas <igor.bernardes2@gmail.com>
2025-08-16 16:55:07 -0300

    feat; adicionado componente de Form, estilos e props

---
+++ frontend/src/types/addcars/types.ts
@@ -0,0 +1,14 @@
+import type { ChangeEvent, FormEvent } from "react";
+export type PropsForm = {
+  form: {
+    marca: string;
+    modelo: string;
+    ano: string;
+  };
+  setForm: (form: PropsForm["form"]) => void;
+  imagem: File | null;
+  setImagem: (imagem: File | null) => void;
+  handleSubmit: (e: FormEvent<HTMLFormElement>) => void | Promise<void>;
+  handleInputChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  handleFileChange: (e: ChangeEvent<HTMLInputElement>) => void;
+};
